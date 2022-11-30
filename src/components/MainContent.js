f9678e28c13865b1990a8b13a71eabe9e9102615
Brandon-Schefstad <bschefstad@gmail.com>
2022-11-30 12:18:19 -0500

    Restyling main page with hero and high contrast.

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,22 +5,23 @@ function MainContent() {
 	return (
 		<section className="mainContent">
 			<h1 className="mainContent__title">Raising Our Bar</h1>
-			<section className="mainContent__description">
-				<p>
-					Philadelphia, unlike other cities, elects their judges.
-					However, information about judges is often difficult to
-					find. This dashboard provides a look at patterns in judge
-					sentencing, bail, and history. "Vocabulary" explains common
-					legal vocabulary as well as some more information about the
-					stats. "Search Judge" lets you select a judge's name and see
-					their history. "Compare Judges" allows you to see two of
-					them side-by-side.
-				</p>
-			</section>
+			<div className="hero">
+				<section className="mainContent__description">
+					<p>
+						Philadelphia, unlike other cities, elects their judges. However,
+						information about judges is often difficult to find. This dashboard
+						provides a look at patterns in judge sentencing, bail, and history.
+						"Vocabulary" explains common legal vocabulary as well as some more
+						information about the stats. "Search Judge" lets you select a
+						judge's name and see their history. "Compare Judges" allows you to
+						see two of them side-by-side.
+					</p>
+				</section>
+			</div>
 
-			<section className="img-box">
+			{/* <section className="img-box">
 				<img src="https://images.law.com/contrib/content/uploads/sites/402/2022/03/Philadelphia-Criminal-Justice-Center-08-767x633.jpg" />
-			</section>
+			</section> */}
 		</section>
 	);
 }
