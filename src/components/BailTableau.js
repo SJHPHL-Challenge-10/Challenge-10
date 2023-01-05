2855fdeff21cdba7c11e191f1199d9f047d737e8
Brandon-Schefstad <bschefstad@gmail.com>
2023-01-04 23:33:24 -0500

    small bugfix on BailTableau

--- src/components/BailTableau.js
+++ src/components/BailTableau.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import TableauReport from 'tableau-react';
-import Header from './Header';
+import React from 'react'
+import TableauReport from 'tableau-react'
+import Header from './Header'
 
 const JudgeTableau = () => {
-	let url = `https://public.tableau.com/views/AverageBailAmounts/AverageBailDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link`;
+	let url = `https://public.tableau.com/views/AverageBailAmounts/AverageBailDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link`
 	return (
 		<div className="App">
 			<Header />
 			<TableauReport url={url} />
 		</div>
-	);
-};
+	)
+}
 
-export default JudgeTableau;
+export default JudgeTableau
--- src/components/DemographicsTableau.jsx
+++ src/components/DemographicsTableau.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import JudgeSelect from './JudgeSelect';
-import Header from './Header';
+import React from 'react'
+import TableauReport from 'tableau-react'
+import Header from './Header'
 
 const DemogrpahicsTableau = () => {
-	let urlToSend = `https://public.tableau.com/views/DemographicsandDocketInformation/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link`;
+	let urlToSend = `https://public.tableau.com/views/DemographicsandDocketInformation/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link`
 	return (
 		<>
 			<Header />
-			<JudgeSelect url={urlToSend} />
+			{/* <JudgeSelect url={urlToSend} /> */}
+			<TableauReport url={urlToSend} />
 		</>
-	);
-};
+	)
+}
 
-export default DemogrpahicsTableau;
+export default DemogrpahicsTableau
